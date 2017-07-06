0f8db35d578b3b214efc53abd456f8d926764064
Konstantin Gorodetski <kgorodetsky@yandex.ru>
2017-07-06 22:09:52 +0300

    Add repo and electron support

--- app/renderer/src/ts/index.tsx
+++ app/renderer/src/ts/index.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 
-import {Main} from "./Main";
+import { Main } from "./Main";
 
 ReactDOM.render(
   <Main compiler="TypeScript" framework="React" />,
---
+++ config/webpack.main.config.js
@@ -0,0 +1,50 @@
+const webpack = require('webpack');
+const path = require('path');
+const HtmlPlugin = require('html-webpack-plugin');
+const { CheckerPlugin } = require('awesome-typescript-loader')
+
+const root = path.resolve('./');
+const rendererPath = `${root}/app/renderer/src`;
+const isDev = process.env.NODE_ENV === 'development';
+
+module.exports = {
+	entry: {
+		renderer: `${rendererPath}/ts/index.tsx`,
+		vendor: ['react', 'react-dom']
+	},
+	output: {
+		path: `${root}/dist`,
+		filename: '[name].js',
+		publicPath: '/'
+	},
+
+	devServer: {
+		port: 3000,
+		compress: true
+	},
+
+	// Enable sourcemaps for debugging webpack's output.
+	devtool: isDev && 'source-map',
+	resolve: {
+		// Add '.ts' and '.tsx' as resolvable extensions.
+		extensions: [".ts", ".tsx", ".js"]
+	},
+	plugins: [
+		new webpack.HotModuleReplacementPlugin(),
+		new HtmlPlugin({
+			chunks: ['renderer', 'vendor'],
+			filename: 'index.html',
+			template: `${rendererPath}/public/index.html`
+		}),
+		new CheckerPlugin(),
+		new webpack.optimize.CommonsChunkPlugin('renderer'),
+	],
+	module: {
+		rules: [
+			// All files with a '.ts' or '.tsx' extension will be handled by 'awesome-typescript-loader'.
+			{ test: /\.tsx?$/, use: ["awesome-typescript-loader"] },
+			// All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
+			{ test: /\.js$/, enforce: "pre", use: "source-map-loader" }
+		]
+	}
+};
