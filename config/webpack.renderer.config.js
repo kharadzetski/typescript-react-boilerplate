2693c68df20386186a86071c93dc30acda00121f
Konstantin Gorodetski <kgorodetsky@yandex.ru>
2017-07-06 20:59:36 +0300

    Source-map only on dev

--- config/webpack.renderer.config.js
+++ config/webpack.renderer.config.js
@@ -1,9 +1,11 @@
 const webpack = require('webpack');
 const path = require('path');
 const HtmlPlugin = require('html-webpack-plugin');
+const { CheckerPlugin } = require('awesome-typescript-loader')
 
 const root = path.resolve('./');
 const rendererPath = `${root}/app/renderer/src`;
+const isDev = process.env.NODE_ENV === 'development';
 
 module.exports = {
 	entry: {
@@ -17,7 +19,7 @@ module.exports = {
 	},
 
 	// Enable sourcemaps for debugging webpack's output.
-	devtool: "source-map",
+	devtool: isDev && 'source-map',
 	resolve: {
 		// Add '.ts' and '.tsx' as resolvable extensions.
 		extensions: [".ts", ".tsx", ".js"]
@@ -29,12 +31,13 @@ module.exports = {
 			chunks: ['renderer', 'vendor'],
 			filename: 'index.html',
 			template: `${rendererPath}/public/index.html`
-		})
+		}),
+		new CheckerPlugin()
 	],
 	module: {
 		rules: [
-			// All files with a '.ts' or '.tsx' extension will be handled by 'ts-loader'.
-			{ test: /\.tsx?$/, use: ["ts-loader"] },
+			// All files with a '.ts' or '.tsx' extension will be handled by 'awesome-typescript-loader'.
+			{ test: /\.tsx?$/, use: ["awesome-typescript-loader"] },
 			// All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
 			{ test: /\.js$/, enforce: "pre", use: "source-map-loader" }
 		]
